469110bfb9cbc7962b7e2dbbaa342b48bd8aca70
sandihidayat <sandihidayat174@gmailcom>
2023-06-02 20:51:50 +0700

    kerjakan di folder masing-masing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,6 +8,33 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
+Cypress.Commands.add('ketik', (locator,value) =>{
+    cy.get(locator)
+    .should('be.visible')
+    .type(value)
+})
+// untuk memanggil login -> cy.login()
+Cypress.Commands.add('login', ()=>{
+    cy.visit('https://opensource-demo.orangehrmlive.com')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin') //username
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123') //password
+    cy.get('.oxd-button').click() //login button
+})
+// untuk ke menu PIM -> cy.menuPim()
+Cypress.Commands.add('menuPim', ()=>{
+    cy.get(':nth-child(2) > .oxd-main-menu-item').click() //menu pim
+})
+// untuk ke menu Add Employee -> cy.menuAddEmploye()
+Cypress.Commands.add('menuAddEmployee', ()=>{
+    cy.get(':nth-child(2) > .oxd-main-menu-item').click() //menu pim
+    cy.get(':nth-child(3) > .oxd-topbar-body-nav-tab-item').click() //menu add employee
+})
+
+// Yang lainnya buat disini sesuai kebutuhan masing-masing
+
+
+
+
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
