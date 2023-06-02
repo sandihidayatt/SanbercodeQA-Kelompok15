fa7d57440a890d40c77b87df3c1ed2bae87f032b
sandihidayat <sandihidayat174@gmailcom>
2023-06-03 06:30:49 +0700

    membuat page object AddEmployee

--- cypress/e2e/PimEmployee/AddEmployee.cy.js
+++ cypress/e2e/PimEmployee/AddEmployee.cy.js
@@ -1,5 +1,5 @@
-import PageAddEmployee from "../../support/PageObject/PageAddEmployee.cy"
-const dataEmployee = require("../../fixtures/OrangeHrm/employee.json")
+import PageAddEmployee from "../../support/PageObject/PageAddEmployee.cy" //memanggil file PageAddEmployee
+const dataEmployee = require("../../fixtures/OrangeHrm/employee.json") //memanggil json data employee
 
 describe('Add Employee Fuctionality', () => { // test suite
   const AddEmployee = new PageAddEmployee
@@ -10,38 +10,60 @@ describe('Add Employee Fuctionality', () => { // test suite
       AddEmployee.inputMiddleName(dataEmployee.middleName) // input middle name
       AddEmployee.inputLastName(dataEmployee.lastName) // input last name
       AddEmployee.inputidEmployee(dataEmployee.idEmployee) // input id employee
-      cy.get('.oxd-button--secondary').click() //save
-      cy.get('.oxd-text--toast-message').should('be.visible') //message success
-      cy.get('.orangehrm-edit-employee-name > .oxd-text').should('be.visible')//halaman profil
-      cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').should('have.value', 'Iris'); //value txtfield firstname
-      cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').should('have.value', 'Rose') //value txtfield lastname  
+      AddEmployee.clickSave() // click save
+      AddEmployee.messageSuccess() // check pesan success
+      AddEmployee.checkProfil() // check profil
+      AddEmployee.checkFirstName() // check value txtfield first name
+      AddEmployee.checkLastName() // check value textfield last name
     })
 
     it('Add Employee Empty FirstName', () => {
       cy.login() // panggil login
       cy.menuAddEmployee() // menuju halaman add employee
-      cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type(' ') //kosongkan firstname
-      cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('Jasmine') //middleName
-      cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type('Iris') //lastname
-      cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('058881') //id employee
-      cy.get('.oxd-button--secondary').click() //save
-      cy.get('.--name-grouped-field > :nth-child(1) > .oxd-text').should('be.visible') //username required
-      cy.url().should('include','/web/index.php/pim/addEmployee') //url form add employee
+      AddEmployee.inputFirstName(dataEmployee.emptyFirstName) // input empty first name
+      AddEmployee.inputMiddleName(dataEmployee.middleName) // input middle name
+      AddEmployee.inputLastName(dataEmployee.lastName) // input last name
+      AddEmployee.inputidEmployee(dataEmployee.idEmployee) // input id employee
+      AddEmployee.clickSave() // click btn save
+      AddEmployee.checkRequiredFirstName()// check required firstname
+      AddEmployee.checkUrl() //check url
     })
 
-    //   it('Add Employee Empty LastName', () => {
-    //   cy.login() // panggil login
-    //   cy.menuAddEmployee() // menuju halaman add employee
-    //   cy.get('.orangehrm-card-container > .oxd-text--h6').should('be.visible') //halaman add employee
-    //   cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type('Iris') //firtname
-    //   cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('Jasmine') //middleName
-    //   cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type(' ') //kosongkan lastname
-    //   cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('058881') //id employee
-    //   cy.get('.oxd-button--secondary').click() //save
-    //   cy.get('.--name-grouped-field > :nth-child(3) > .oxd-text').should('be.visible') //lastname required
-    //   cy.url().should('include','/web/index.php/pim/addEmployee') //url halaman add employee
-    // })
-  
+      it('Add Employee Empty LastName', () => {
+      cy.login() // panggil login
+      cy.menuAddEmployee() // menuju halaman add employee
+      AddEmployee.inputFirstName(dataEmployee.firstName) // input first name
+      AddEmployee.inputMiddleName(dataEmployee.middleName) // input middle name
+      AddEmployee.inputLastName(dataEmployee.emptyLastName) // input empty last name
+      AddEmployee.inputidEmployee(dataEmployee.idEmployee) // input id employee
+      AddEmployee.clickSave() // click btn save
+      AddEmployee.checkRequiredLastName()// check required lastname
+      AddEmployee.checkUrl() //check url
+    })
+
+    it('Add Employee Empty FirstName and LastName', () => {
+      cy.login() // panggil login
+      cy.menuAddEmployee() // menuju halaman add employee
+      AddEmployee.inputFirstName(dataEmployee.emptyFirstName) // input empty first name
+      AddEmployee.inputMiddleName(dataEmployee.middleName) // input middle name
+      AddEmployee.inputLastName(dataEmployee.emptyLastName) // input empty last name
+      AddEmployee.inputidEmployee(dataEmployee.idEmployee) // input id employee
+      AddEmployee.clickSave() // click btn save
+      AddEmployee.checkRequiredFirstName()// check required firstname
+      AddEmployee.checkRequiredLastName()// check required lastname
+      AddEmployee.checkUrl() //check url
+    })
+
+    it('Cancle Add Employee', () => {
+      cy.login() // panggil login
+      cy.menuAddEmployee() // menuju halaman add employee
+      AddEmployee.inputFirstName(dataEmployee.firstName) // input empty first name
+      AddEmployee.inputMiddleName(dataEmployee.middleName) // input middle name
+      AddEmployee.inputLastName(dataEmployee.lastName) // input empty last name
+      AddEmployee.inputidEmployee(dataEmployee.idEmployee) // input id employee
+      AddEmployee.clickCancle() // click btn cancle
+      AddEmployee.checkUrlCancle() //check url
+    })
   })
   
   
--- cypress/support/PageObject/PageAddEmployee.cy.js
+++ cypress/support/PageObject/PageAddEmployee.cy.js
@@ -1,29 +1,61 @@
-class PageAddEmployee {
-    firstName = '.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input'
-    middleName = ':nth-child(2) > :nth-child(2) > .oxd-input'
-    lastName = ':nth-child(3) > :nth-child(2) > .oxd-input'
-    idEmployee = '.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input'
-    btnSave = '.oxd-button--secondary'
-    msgSucces = '.oxd-text--toast-message'
-    profil = '.orangehrm-edit-employee-name > .oxd-text'
-    valueFirstName = '.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input'
-    valueLastName = ':nth-child(3) > :nth-child(2) > .oxd-input'
+const dataEmployee = require("../../fixtures/OrangeHrm/employee.json")
 
-    inputFirstName(firstName) {
+class PageAddEmployee {
+    firstName = '.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input' // locator firstname
+    middleName = ':nth-child(2) > :nth-child(2) > .oxd-input' // locator middle name
+    lastName = ':nth-child(3) > :nth-child(2) > .oxd-input' // locator lastname
+    idEmployee = '.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input' // locator id employee
+    btnSave = '.oxd-button--secondary' //locator btn save
+    msgSuccess = '.oxd-text--toast-message' // locator message success
+    profil = '.orangehrm-edit-employee-name > .oxd-text' //locator profil
+    requiredFirstName = '.--name-grouped-field > :nth-child(1) > .oxd-text' //locator required first name
+    requiredLastName = '.--name-grouped-field > :nth-child(3) > .oxd-text' // locator last name
+    urlAddEmployee = '/web/index.php/pim/addEmployee' // url halaman add employee
+    btnCancle = '.oxd-button--ghost' // locator btn cancle
+    urlCancle = '/web/index.php/pim/viewEmployeeList' // locator url menu pim jika cancle
+
+    inputFirstName(firstName) { //fungsi input textfield firstname
         cy.input(this.firstName,firstName)
     }
-    inputMiddleName(middleName) {
+    inputMiddleName(middleName) { //fungsi input textfield middle name
         cy.input(this.middleName,middleName)
     }
-    inputLastName(lastName) {
+    inputLastName(lastName) { //fungsi input textfield last name
         cy.input(this.lastName,lastName)
     }
-    inputidEmployee(idEmployee) {
+    inputidEmployee(idEmployee) { //fungsi input textfield id employee
         cy.input(this.idEmployee,idEmployee)
     }
-
-
-
+    clickSave() { //fungsi click save
+        cy.get(this.btnSave).click()
+    }
+    messageSuccess() { //fungsi cek message success
+        cy.get(this.msgSuccess).should('be.visible')
+    }
+    checkProfil() { //fungsi cek profil
+        cy.get(this.profil).should('be.visible')
+    }
+    checkFirstName() { //fungsi check value txtfield firstname
+        cy.get(this.firstName).should('have.value',dataEmployee.firstName)
+    }
+    checkLastName() { //fungsi check value txtfield lastname
+        cy.get(this.lastName).should('have.value',dataEmployee.lastName)
+    }
+    checkRequiredFirstName() { // fungsi check pesan required firstname
+        cy.get(this.requiredFirstName).should('be.visible')
+    }
+    checkRequiredLastName() { // fungsi check pesan required lastname
+        cy.get(this.requiredLastName).should('be.visible')
+    }
+    checkUrl() { // fungsi check url add employee
+        cy.url().should('include',this.urlAddEmployee)
+    }
+    clickCancle() { //fungsi click cancle
+        cy.get(this.btnCancle).click()
+    }
+    checkUrlCancle() { // fungsi check url cancle
+        cy.url().should('include',this.urlCancle)
+    }
 }
 
-export default PageAddEmployee
+export default PageAddEmployee // supaya bisa dipanggil ke hlmn/file lain
