85945add32be50ed6d58c803ca225037be6e9633
sandihidayat <sandihidayat174@gmailcom>
2023-06-02 21:40:30 +0700

    tambah AddEmployee

---
+++ cypress/e2e/PimEmployee/AddEmployee.cy.js
@@ -0,0 +1,44 @@
+describe('Add Employee Fuctionality', () => {
+    it('Success Add Employee', () => {
+      cy.login() // panggil login
+      cy.menuAddEmployee() // menuju halaman add employee
+      cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type('Iris') //firtname
+      cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('Jasmine') //middleName
+      cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type('Rose') //lastname
+      cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('058881') //id employee
+      cy.get('.oxd-button--secondary').click() //save
+      cy.get('.oxd-text--toast-message').should('be.visible') //message success
+      cy.get('.orangehrm-edit-employee-name > .oxd-text').should('be.visible')//halaman profil
+      cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').should('have.value', 'Iris'); //value txtfield firstname
+      cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').should('have.value', 'Rose') //value txtfield lastname  
+    })
+
+    it('Add Employee Empty FirstName', () => {
+      cy.login() // panggil login
+      cy.menuAddEmployee() // menuju halaman add employee
+      cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type(' ') //kosongkan firstname
+      cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('Jasmine') //middleName
+      cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type('Iris') //lastname
+      cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('058881') //id employee
+      cy.get('.oxd-button--secondary').click() //save
+      cy.get('.--name-grouped-field > :nth-child(1) > .oxd-text').should('be.visible') //username required
+      cy.url().should('include','/web/index.php/pim/addEmployee') //url form add employee
+    })
+
+      it('Add Employee Empty LastName', () => {
+      cy.login() // panggil login
+      cy.menuAddEmployee() // menuju halaman add employee
+      cy.get('.orangehrm-card-container > .oxd-text--h6').should('be.visible') //halaman add employee
+      cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type('Iris') //firtname
+      cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type('Jasmine') //middleName
+      cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type(' ') //kosongkan lastname
+      cy.get('.oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('058881') //id employee
+      cy.get('.oxd-button--secondary').click() //save
+      cy.get('.--name-grouped-field > :nth-child(3) > .oxd-text').should('be.visible') //lastname required
+      cy.url().should('include','/web/index.php/pim/addEmployee') //url halaman add employee
+    })
+  
+  })
+  
+  
+  
